96bbfe2c655f6ccdb9033a3bc227750e8cfb89a1
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-07-19 09:24:21 +0300

    Коміт

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,10 @@
 // его текущее значение в span#name - output.Если инпут пустой,
 //     в спане должна отображаться строка 'незнакомец'.
 
-{/* <input type="text" placeholder="Ваше имя?" id="name-input" />
-<h1>Привет, <span id="name-output">незнакомец</span>!</h1>  */}
+// { <input type="text" placeholder="Ваше имя?" id="name-input" />
+// <h1>Привет, <span id="name-output">незнакомец</span>!</h1> }
+
+function input() {
+  const input_taker = document.getElementById('name-input').value;
+  document.getElementById('name-output').innerHTML = input_taker;
+}
--- js/task-06.js
+++ js/task-06.js
@@ -24,3 +24,15 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+
+document.getElementById('validation-input').onblur = function() {
+    console.log(this.value.length);
+    if((this.getAttribute('data-length') > this.value.length) || (this.getAttribute('data-length') < this.value.length)) {
+        this.classList.remove('valid');
+        this.classList.add('invalid');
+    }
+    else {
+        this.classList.remove('invalid');
+        this.classList.add('valid');
+    }
+}
