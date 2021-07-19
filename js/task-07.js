b45deceffa603af7dec973a66b5a91072498b1ca
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-07-19 10:42:17 +0300

    commit

--- js/task-07.js
+++ js/task-07.js
@@ -8,12 +8,8 @@
 // <br />
 // <span id="text">Абракадабра!</span>
 
-// const imputRangerEl = document.querySelector('#font-size-color')
-// const textEl = document.querySelector('#text')
-
-// const changeFontSizeText = inputRangeEl.addEventListener("input", onChangeSizeText)
-
-// function onChangeSizeText(event) {
-//     const fontSize = Number(event.currentTarget.value)
-//     textEl.setAttribute("style", `font-size: ${fontSize}`)
-// }
+// "use strict";
+// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет// инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+// <input id="font-size-control" type="range" />// <br />// <span id="text">Абракадабра!</span>
+let text = document.getElementById("text");
+controlFont.oninput = function() {  text.style.fontSize = controlFont.value + "px";};
